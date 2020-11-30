da524b5f46dbedb8ebb9735398c85bc445e8ad26
Yu-7053 <61251255+Yu-7053@users.noreply.github.com>
2020-11-30 12:55:52 +0100

    Add files via upload
    babel exercise

---
+++ Exercise/learn-babel/dist/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var myArray = ['a', 'b', 'c'];
+var one = myArray[0],
+    two = myArray[1],
+    three = myArray[2];
+
+console.log(one, two, three);
+
+// example with an object 
+
+var obj = {
+    one1: {
+        a: 'a'
+    },
+    two2: {
+        b: 'b'
+    },
+    three3: {
+        c: 'c'
+    }
+
+};
+
+var one1 = obj.one1,
+    two2 = obj.two2,
+    three3 = obj.three3;
+
+
+console.log(one1);
